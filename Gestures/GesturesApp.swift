950dc48404aa7f4ec17f00862b177187910f8300
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-22 08:37:07 +0530

    Initial Commit

---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by shehan karunarathna on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
